2183bcf8fba3dbd3147dbef512b52e73a233b80a
elizabeth011685 <elizabeth011685@hotmail.com>
2024-10-08 11:58:33 -0600

    Add refs and refactor setup script to Composition API
    Enhanced the Vue component by adding `ref` attributes to `TreeSelect` instances for direct references. Refactored the script section to use the Composition API with reactive variables and updated method definitions and watchers accordingly.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="heading">Single Value</div>
 
-    <Treeselect v-model="test" :options="options"
+    <Treeselect v-model="test" :options="options" ref="myTreeSelect"
                 @open="e => handleEvent(e, 'open')"
                 @close="e => handleEvent(e, 'close')"
                 :clear-on-select="false"
@@ -11,11 +11,12 @@
                 @deselect="e => handleEvent(e, 'deselect')"
                 @search-change="e => handleEvent(e, 'search-change')"
                 :close-on-select="false"
+
     />
 
     <div class="heading">Multiple Values</div>
 
-    <Treeselect v-model="test2" :options="options"
+    <Treeselect v-model="test2" :options="options" ref="myTreeSelect2"
                 :multiple="true"
                 :close-on-select="false"
                 @open="e => handleEvent(e, 'open')"
@@ -28,7 +29,7 @@
 
     <div class="heading">Slots</div>
 
-    <Treeselect v-model="test3" :options="options"
+    <Treeselect v-model="test3" :options="options" ref="myTreeSelect3"
                 @open="e => handleEvent(e, 'open')"
                 @close="e => handleEvent(e, 'close')"
                 :multiple="true"
@@ -55,98 +56,113 @@
 
     <div class="heading">Async</div>
 
-    <Treeselect v-model="test4"
+    <Treeselect v-model="test4" ref="myTreeSelect4"
                 :async="true"
                 :default-options="true"
                 :load-options="loadOptions"
     />
   </div>
   <button @click="clear">Limpia</button>
-  <button @click="imprime">Imprime</button>
 </template>
 
-<script>
+<script setup>
+import { ref, toRaw, watch } from 'vue';
 import Treeselect from "@/components/Treeselect.vue";
-import { toRaw } from "vue";
 
-export default {
-  components: { Treeselect },
-  data() {
-    return {
-      test: 'b',
-      test2: ['b', 'c'],
-      test3: ['b', 'c'],
-      test4: null,
-      options: [ {
-        id: 'a',
-        label: 'Ohm',
-        symbol: "&#8486;",
-        children: [ {
-          id: 'aa',
-          label: 'Beta',
-          symbol: "&#8492;"
-        }, {
-          id: 'ab',
-          label: 'Charlie',
-          symbol: "&#8493;"
-        } ],
-      }, {
-        id: 'b',
-        label: 'Snowman',
-        symbol: '&#9731;',
-      }, {
-        id: 'c',
-        label: 'Phone',
-        symbol: '&#9743;'
-      } ],
-      highLevelOptions: [ {
-        id: 'a1',
-        label: 'Ohm-lvl1',
-        symbol: "&#8486;",
-        children: null,
-      }],
-    }
-  },
-  methods: {
-    handleEvent(event, type) {
-      console.log(event, type);
-    },
-    fakeLoad() {
-      return new Promise((res) => {
-        setTimeout(() => res(toRaw(this.highLevelOptions)),2000)
-      })
-    },
-    fakeLoadChildren() {
-      return new Promise((res) => {
-        setTimeout(() => res(toRaw(this.options)),2000)
-      })
-    },
-    async loadOptions({ action, searchQuery, callback }) {
-      if (action === 'ASYNC_SEARCH') {
-        const options = await this.fakeLoad();
-        callback(null, options);
+// Variables reactivas
+const test = ref('b');
+const test2 = ref(['b', 'c']);
+const test3 = ref(['b', 'c']);
+const test4 = ref(null);
+
+const options = ref([
+  {
+    id: 'a',
+    label: 'Ohm',
+    symbol: "&#8486;",
+    children: [
+      {
+        id: 'aa',
+        label: 'Beta',
+        symbol: "&#8492;"
+      },
+      {
+        id: 'ab',
+        label: 'Charlie',
+        symbol: "&#8493;"
       }
-      if (action === 'LOAD_CHILDREN_OPTIONS') {
-        const options = await this.fakeLoadChildren();
-        callback(null, options);
-      }
-    },
-    imprime(){
-      console.log(this.test);
-    },
-    clear() {
-      this.test = null;
-    }
+    ],
   },
-  watch: {
-    test(val, oVal) {
-      console.log(val, oVal);
-    },
-    test2(val, oVal) {
-      console.log(val, oVal);
-    }
+  {
+    id: 'b',
+    label: 'Snowman',
+    symbol: '&#9731;',
+  },
+  {
+    id: 'c',
+    label: 'Phone',
+    symbol: '&#9743;'
+  }
+]);
+
+const highLevelOptions = ref([
+  {
+    id: 'a1',
+    label: 'Ohm-lvl1',
+    symbol: "&#8486;",
+    children: null,
+  }
+]);
+
+// Referencia al componente
+const myTreeSelect = ref(null);
+const myTreeSelect2 = ref(null);
+const myTreeSelect3 = ref(null);
+const myTreeSelect4 = ref(null);
+
+// Métodos
+const handleEvent = (event, type) => {
+  console.log(event, type);
+};
+
+const fakeLoad = () => {
+  return new Promise((res) => {
+    setTimeout(() => res(toRaw(highLevelOptions.value)), 2000);
+  });
+};
+
+const fakeLoadChildren = () => {
+  return new Promise((res) => {
+    setTimeout(() => res(toRaw(options.value)), 2000);
+  });
+};
+
+const loadOptions = async ({ action, searchQuery, callback }) => {
+  if (action === 'ASYNC_SEARCH') {
+    const options = await fakeLoad();
+    callback(null, options);
+  }
+  if (action === 'LOAD_CHILDREN_OPTIONS') {
+    const options = await fakeLoadChildren();
+    callback(null, options);
   }
 };
+
+const clear = () => {
+  myTreeSelect.value?.clear();
+  myTreeSelect2.value?.clear();
+  myTreeSelect3.value?.clear();
+  myTreeSelect4.value?.clear();
+};
+
+// Watchers
+watch(test, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
+});
+
+watch(test2, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
+});
 </script>
 
 <style lang="scss">
